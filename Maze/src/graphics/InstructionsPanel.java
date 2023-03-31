067b8e321f04757bdc084ed8044ecfa75b3673af
Jason Cheng <dumplinglife7584@gmail.com>
2023-03-30 22:01:38 -0700

    fix typo

--- Maze/src/graphics/InstructionsPanel.java
+++ Maze/src/graphics/InstructionsPanel.java
@@ -23,7 +23,7 @@ public class InstructionsPanel extends JPanel {
                 "<br />L: turn left (i.e. turn counterclockwise)" +
                 "<br />R: turn right (i.e. turn clockwise)" +
                 "<br />F2: forward 2" +
-                "<br />There may be boulders in the way. Every move has an attached stamina value (0 by default). 50 stamina is needed to push 1 boulder. If 2 boulders are adjacent, the robot cna push both with 100 stamina. You are in charge of calculating when stamina is needed and how much should be used." +
+                "<br />There may be boulders in the way. Every move has an attached stamina value (0 by default). 50 stamina is needed to push 1 boulder. If 2 boulders are adjacent, the robot can push both with 100 stamina. You are in charge of calculating when stamina is needed and how much should be used." +
                 "<br />The syntax for moving with stamina is:" +
                 "<br />F2_50: forward 2 with stamina 50 on both movements" +
                 "</html>");
