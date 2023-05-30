3b28e30cd63391a125528a9233551fbd2e5148e7
Jason Cheng <dumplinglife7584@gmail.com>
2023-05-30 10:27:29 -0700

    custom level

--- Maze/src/graphics/MainView.java
+++ Maze/src/graphics/MainView.java
@@ -3,6 +3,11 @@ package graphics;
 import core.*;
 
 import java.awt.CardLayout;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import javax.swing.*;
 
 public class MainView {
@@ -10,48 +15,76 @@ public class MainView {
     private JPanel cards;
     private GamePage gamePage;
     private int level;
-    private MazeTemplate[] levelTemplates = new MazeTemplate[] {
-        //L F F R F F F F R F F L F F F F L F F R F F F
-        new MazeTemplate(new String[] {
-            "############",
-            "#.....#.....",
-            "#.###.#.#.##",
-            "#P#.......##",
-            "############",
-        }, 0),
-        //
-        new MazeTemplate(new String[] {
-            "############",
-            "#.o.........",
-            "#.###.#.#.##",
-            "#P#.......##",
-            "############",
-        }, 4 * Maze.STAMINA_FOR_ONE_BOULDER),
-        //L F R F R F3_50 L F2 L F F_50 R F2
-        new MazeTemplate(new String[] {
-            "######",
-            "#..#.#",
-            "#Po#o.",
-            "##.#.#",
-            "##...#",
-            "##.###",
-            "######",
-        }, 6 * Maze.STAMINA_FOR_ONE_BOULDER), //actually only needs 4 stamina
-        new MazeTemplate(new String[] {
-            "#########",
-            "#..o....#",
-            "#.#o#####",
-            "#.#o#####",
-            "#P#o....#",
-            "#.#o###.#",
-            "#.#o###.#",
-            "#.#o###.#",
-            "#...###.#",
-            "#######.#",
-        }, 29 * Maze.STAMINA_FOR_ONE_BOULDER), //best way is 29 stamina, I think
-    };
+    private MazeTemplate[] levelTemplates;
 
     public MainView() {
+    	//read level.txt, then put it into levelTemplates
+    	Scanner scan = null;
+    	try {
+			scan = new Scanner(new File("level.txt"));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+    	int customLevelStamina = Integer.parseInt(scan.nextLine());
+    	ArrayList<String> customLevelGridList = new ArrayList<>();
+    	while(scan.hasNext()) {
+    		customLevelGridList.add(scan.nextLine());
+    	}
+    	String[] customLevelGrid = customLevelGridList.toArray(new String[0]);
+    	
+    	
+    	levelTemplates = new MazeTemplate[] {
+	        //L F F R F F F F R F F L F F F F L F F R F F F
+	        new MazeTemplate(new String[] {
+	            "############",
+	            "#.....#.....",
+	            "#.###.#.#.##",
+	            "#P#.......##",
+	            "############",
+	        }, 0),
+	        //
+	        new MazeTemplate(new String[] {
+	            "############",
+	            "#.o.........",
+	            "#.###.#.#.##",
+	            "#P#.......##",
+	            "############",
+	        }, 4 * Maze.STAMINA_FOR_ONE_BOULDER),
+	        //L F R F R F3_50 L F2 L F F_50 R F2
+	        new MazeTemplate(new String[] {
+	            "######",
+	            "#..#.#",
+	            "#Po#o.",
+	            "##.#.#",
+	            "##...#",
+	            "##.###",
+	            "######",
+	        }, 6 * Maze.STAMINA_FOR_ONE_BOULDER), //actually only needs 4 stamina
+	        new MazeTemplate(new String[] {
+	            "#########",
+	            "#..o....#",
+	            "#.#o#####",
+	            "#.#o#####",
+	            "#P#o....#",
+	            "#.#o###.#",
+	            "#.#o###.#",
+	            "#.#o###.#",
+	            "#...###.#",
+	            "#######.#",
+	        }, 29 * Maze.STAMINA_FOR_ONE_BOULDER), //best way is 29 stamina, I think
+	        //for screenshots
+	        /*
+	        new MazeTemplate(new String[] {
+	            "#######",
+	            "#.Poo.#",
+	            "#######",
+	        }, 4 * Maze.STAMINA_FOR_ONE_BOULDER), //actually only needs 4 stamina
+	        */
+	        
+	        new MazeTemplate(customLevelGrid, customLevelStamina),
+	    };
+    	
+    	
         frame = new JFrame("Start Page");
         cards = new JPanel(new CardLayout());
         StartPanel startPanel = new StartPanel(
